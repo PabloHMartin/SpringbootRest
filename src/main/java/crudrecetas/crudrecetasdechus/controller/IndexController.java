60b5d607f528f622976b814af57c42d3dccc030e
PabloHMartin <hermar65@gmail.com>
2018-07-11 18:20:45 +0200

    preheroku

--- src/main/java/crudrecetas/crudrecetasdechus/controller/IndexController.java
+++ src/main/java/crudrecetas/crudrecetasdechus/controller/IndexController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 public class IndexController {
 
-    private final String INDEX_WEB_VIEW = "index";
+    private final String INDEX_WEB_VIEW = "crudindex";
 
     @GetMapping("/")
     public String index(){
